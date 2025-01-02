d834b1774655e081e3674b0c94d12f827181aa16
Ye Seul Kim <yskim940914@gmail.com>
2025-01-02 23:01:44 +0900

    chore: supabase 설정 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-	return <div></div>;
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Countries() {
+	const supabase = await createClient();
+	const { data: countries } = await supabase.from('countries').select();
+
+	return <pre>{JSON.stringify(countries, null, 2)}</pre>;
 }
---
+++ src/utils/supabase/middleware.ts
@@ -0,0 +1,4 @@
+// import { createServerClient } from "@supabase/ssr";
+// import { type NextRequest, NextResponse } from 'next/server';
+
+export const updateSession = async () => {};
